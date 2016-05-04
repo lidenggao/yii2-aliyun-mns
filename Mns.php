913c5c7ccadc20b479ec86ede620bc43d91fba8a
colee <5969226@qq.com>
2016-05-04 15:58:34 +0800

    add composer.json

---
+++ Mns.php
@@ -0,0 +1,19 @@
+<?php
+namespace colee\aliyun;
+require_once("/path_to_sdk/mns-autoloader.php");
+use yii\base\Component;
+/**
+ * ==============================================
+ * Copy right 2015-2016
+ * ----------------------------------------------
+ * This is not a free software, without any authorization is not allowed to use and spread.
+ * ==============================================
+ * 阿里云MQ SDK
+ * @param unknowtype
+ * @return return_type
+ * @author: CoLee
+ */
+class Mns extends Component
+{
+    
+}
